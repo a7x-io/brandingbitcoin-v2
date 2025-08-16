4ad1f99507968cb1fbc0ca6812c6aa347f050c89
a7x-io <contacto@estudiounlimited.com>
2025-08-15 21:35:32 -0400

    Fix Twitter pixel: use correct pixel ID 'qcg5j' and proper initialization pattern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,18 +39,16 @@ export default function RootLayout({
         <meta name="msapplication-TileImage" content="/apple-fav.png" />
         
         {/* Twitter Pixel Base Code */}
-        <script async src="https://static.ads-twitter.com/uwt.js"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
             console.log('Twitter Pixel: Initializing...');
-            window.twq = window.twq || function() {
-              console.log('Twitter Pixel: Function called with args:', arguments);
-              (window.twq.q = window.twq.q || []).push(arguments);
-            };
-            console.log('Twitter Pixel: Setting up initialization...');
-            twq('init', 'tw-qcg5j-qcg7r');
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            console.log('Twitter Pixel: Configuring...');
+            twq('config','qcg5j');
             console.log('Twitter Pixel: Tracking page view...');
-            twq('track', 'PageView');
+            twq('track','PageView');
             console.log('Twitter Pixel: Setup complete');
           `
         }} />
