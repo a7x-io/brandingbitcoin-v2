25721953f20e80f8f16f6e6480bf1d76090e7c32
a7x-io <contacto@estudiounlimited.com>
2025-08-20 22:04:52 -0400

    feat: Update theme toggle to show system icon when theme is set to system

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -11,14 +11,27 @@ import {
 } from "./dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  // Function to get the appropriate icon based on current theme
+  const getThemeIcon = () => {
+    switch (theme) {
+      case 'light':
+        return <Sun className="h-[1.2rem] w-[1.2rem]" />
+      case 'dark':
+        return <Moon className="h-[1.2rem] w-[1.2rem]" />
+      case 'system':
+        return <Monitor className="h-[1.2rem] w-[1.2rem]" />
+      default:
+        return <Monitor className="h-[1.2rem] w-[1.2rem]" />
+    }
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" className="h-9 w-9">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {getThemeIcon()}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
